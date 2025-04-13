388cbe42243bf74614a50261006b0c911f6a634e
Juunibyou <danijunn@gmail.com>
2025-04-13 16:24:36 -0500

    Update

--- app/src/test/java/org/example/GamelogTest.java
+++ app/src/test/java/org/example/GamelogTest.java
@@ -3,18 +3,72 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 class GamelogTest {
 
+//UPDATE LOG & PRINT SCORE TEST
     Gamelog gamelog = new Gamelog();
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    PrintStream originalOut = System.out;
 
     @Test
-    void testSaveLog(){
+    void testPrint_Xvictory(){
     gamelog.updateLog('X');
     gamelog.updateLog('X');
 
-    gamelog.saveLog();
+    System.setOut(new PrintStream(outContent));
+
+    gamelog.printWin();
+
+    System.setOut(originalOut);
+    String output = outContent.toString();
+
+    assertTrue(output.contains("Player 'X' wins: 2"));
+    assertTrue(output.contains("Player 'O' wins: 0"));
+    assertTrue(output.contains("Ties: 0"));
     }
 
 
+    @Test
+    void testPrint_Yvictory(){
+    gamelog.updateLog('O');
+    gamelog.updateLog('O');
+    gamelog.updateLog('O');
+
+    System.setOut(new PrintStream(outContent));
+
+    gamelog.printWin();
+
+    System.setOut(originalOut);
+    String output = outContent.toString();
+
+    assertTrue(output.contains("Player 'X' wins: 0"));
+    assertTrue(output.contains("Player 'O' wins: 3"));
+    assertTrue(output.contains("Ties: 0"));
+    }
+
+
+    @Test
+    void testPrint_Tie(){
+    gamelog.updateLog('T');
+
+    System.setOut(new PrintStream(outContent));
+
+    gamelog.printWin();
+
+    System.setOut(originalOut);
+    String output = outContent.toString();
+
+    assertTrue(output.contains("Player 'X' wins: 0"));
+    assertTrue(output.contains("Player 'O' wins: 0"));
+    assertTrue(output.contains("Ties: 1"));
+    }
+
+//
 }
+
+
+
   
